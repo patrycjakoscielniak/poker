7040bf12515e15914564846b5eaa85d976dbdc0b
Patrycja Koscielniak <patrycja.koscielniak.eu@gmail.com>
2023-08-03 14:18:16 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        backgroundColor: Color.fromARGB(255, 49, 102, 51),
+        body: Center(child: Text('Poker')),
+      ),
+    );
+  }
+}
