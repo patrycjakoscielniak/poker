a361d0f5d2a841b55205fe3b890e65c48ef35b39
Patrycja Koscielniak <patrycja.koscielniak.eu@gmail.com>
2023-08-05 13:58:07 +0200

    Create Hand model

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -33,3 +33,24 @@ class Card {
     this.faceUp = false,
   });
 }
+
+class Deck {
+  List<Card> allCards;
+  Deck({required this.allCards});
+}
+
+class Hand {
+  Card card1;
+  Card card2;
+  Card card3;
+  Card card4;
+  Card card5;
+
+  Hand({
+    required this.card1,
+    required this.card2,
+    required this.card3,
+    required this.card4,
+    required this.card5,
+  });
+}
